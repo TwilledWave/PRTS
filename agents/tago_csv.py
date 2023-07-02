ac9f42ceafa9ec83d505612a32fcca4df5aec432
Twilled Wave <you@example.com>
2023-07-02 13:58:49 -0400

    add agent to summarize CSV data

--- agents/tago_csv.py
+++ agents/tago_csv.py
@@ -2,10 +2,10 @@
 csv_location = ''
 
 from langchain.agents import create_csv_agent
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.agents import AgentExecutor
 
-agent = create_csv_agent(OpenAI(temperature=0, model_name = 'gpt-3.5-turbo'), 
-                         '/cache/RePORTER_PRJ_C_FY2022.csv', 
+#use a high token count model gpt-3.5-turbo-16k for csv data
+agent = create_csv_agent(ChatOpenAI(temperature=0, model_name = 'gpt-3.5-turbo-16k'), 
+                         '../cache/RePORTER_PRJ_C_FY2022.csv', 
                          verbose=True)
-
-agent_chain = AgentExecutor.from_agent_and_tools(agent=agent, verbose=True)
